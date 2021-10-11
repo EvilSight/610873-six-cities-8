cdc7b08f3ad1e7d41f7357b324deadd0de157a01
Chibisov Dmitriy <demonzgrad@mail.ru>
2021-10-11 12:56:42 +0300

    устранил ошибки "npm install"

--- project/src/components/welcome-screen/welcome-screen.tsx
+++ project/src/components/welcome-screen/welcome-screen.tsx
@@ -9,8 +9,8 @@ function WelcomeScreen({errorsCount}: WelcomeScreenProps): JSX.Element {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
+              <a className="header__logo-link header__logo-link--active" href="#">
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
               </a>
             </div>
             <nav className="header__nav">
@@ -78,17 +78,17 @@ function WelcomeScreen({errorsCount}: WelcomeScreenProps): JSX.Element {
               <b className="places__found">312 places to stay in Amsterdam</b>
               <form className="places__sorting" action="#" method="get">
                 <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabindex="0">
+                <span className="places__sorting-type" tabIndex={0}>
                   Popular
                   <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlink:href="#icon-arrow-select"></use>
+                    <use xlinkHref="#icon-arrow-select"></use>
                   </svg>
                 </span>
                 <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabindex="0">Popular</li>
-                  <li className="places__option" tabindex="0">Price: low to high</li>
-                  <li className="places__option" tabindex="0">Price: high to low</li>
-                  <li className="places__option" tabindex="0">Top rated first</li>
+                  <li className="places__option places__option--active" tabIndex={0}>Popular</li>
+                  <li className="places__option" tabIndex={0}>Price: low to high</li>
+                  <li className="places__option" tabIndex={0}>Price: high to low</li>
+                  <li className="places__option" tabIndex={0}>Top rated first</li>
                 </ul>
               </form>
               <div className="cities__places-list places__list tabs__content">
@@ -98,7 +98,7 @@ function WelcomeScreen({errorsCount}: WelcomeScreenProps): JSX.Element {
                   </div>
                   <div className="cities__image-wrapper place-card__image-wrapper">
                     <a href="#">
-                      <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image">
+                      <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image" />
                     </a>
                   </div>
                   <div className="place-card__info">
@@ -109,14 +109,14 @@ function WelcomeScreen({errorsCount}: WelcomeScreenProps): JSX.Element {
                       </div>
                       <button className="place-card__bookmark-button button" type="button">
                         <svg className="place-card__bookmark-icon" width="18" height="19">
-                          <use xlink:href="#icon-bookmark"></use>
+                          <use xlinkHref="#icon-bookmark"></use>
                         </svg>
                         <span className="visually-hidden">To bookmarks</span>
                       </button>
                     </div>
                     <div className="place-card__rating rating">
                       <div className="place-card__stars rating__stars">
-                        <span style="width: 80%"></span>
+                        <span style={{width: '80%'}}></span>
                         <span className="visually-hidden">Rating</span>
                       </div>
                     </div>
@@ -130,7 +130,7 @@ function WelcomeScreen({errorsCount}: WelcomeScreenProps): JSX.Element {
                 <article className="cities__place-card place-card">
                   <div className="cities__image-wrapper place-card__image-wrapper">
                     <a href="#">
-                      <img className="place-card__image" src="img/room.jpg" width="260" height="200" alt="Place image">
+                      <img className="place-card__image" src="img/room.jpg" width="260" height="200" alt="Place image" />
                     </a>
                   </div>
                   <div className="place-card__info">
@@ -141,14 +141,14 @@ function WelcomeScreen({errorsCount}: WelcomeScreenProps): JSX.Element {
                       </div>
                       <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
                         <svg className="place-card__bookmark-icon" width="18" height="19">
-                          <use xlink:href="#icon-bookmark"></use>
+                          <use xlinkHref="#icon-bookmark"></use>
                         </svg>
                         <span className="visually-hidden">In bookmarks</span>
                       </button>
                     </div>
                     <div className="place-card__rating rating">
                       <div className="place-card__stars rating__stars">
-                        <span style="width: 80%"></span>
+                        <span style={{width: '80%'}}></span>
                         <span className="visually-hidden">Rating</span>
                       </div>
                     </div>
@@ -162,7 +162,7 @@ function WelcomeScreen({errorsCount}: WelcomeScreenProps): JSX.Element {
                 <article className="cities__place-card place-card">
                   <div className="cities__image-wrapper place-card__image-wrapper">
                     <a href="#">
-                      <img className="place-card__image" src="img/apartment-02.jpg" width="260" height="200" alt="Place image">
+                      <img className="place-card__image" src="img/apartment-02.jpg" width="260" height="200" alt="Place image" />
                     </a>
                   </div>
                   <div className="place-card__info">
@@ -173,14 +173,14 @@ function WelcomeScreen({errorsCount}: WelcomeScreenProps): JSX.Element {
                       </div>
                       <button className="place-card__bookmark-button button" type="button">
                         <svg className="place-card__bookmark-icon" width="18" height="19">
-                          <use xlink:href="#icon-bookmark"></use>
+                          <use xlinkHref="#icon-bookmark"></use>
                         </svg>
                         <span className="visually-hidden">To bookmarks</span>
                       </button>
                     </div>
                     <div className="place-card__rating rating">
                       <div className="place-card__stars rating__stars">
-                        <span style="width: 80%"></span>
+                        <span style={{width: '80%'}}></span>
                         <span className="visually-hidden">Rating</span>
                       </div>
                     </div>
@@ -197,7 +197,7 @@ function WelcomeScreen({errorsCount}: WelcomeScreenProps): JSX.Element {
                   </div>
                   <div className="cities__image-wrapper place-card__image-wrapper">
                     <a href="#">
-                      <img className="place-card__image" src="img/apartment-03.jpg" width="260" height="200" alt="Place image">
+                      <img className="place-card__image" src="img/apartment-03.jpg" width="260" height="200" alt="Place image" />
                     </a>
                   </div>
                   <div className="place-card__info">
@@ -208,14 +208,14 @@ function WelcomeScreen({errorsCount}: WelcomeScreenProps): JSX.Element {
                       </div>
                       <button className="place-card__bookmark-button button" type="button">
                         <svg className="place-card__bookmark-icon" width="18" height="19">
-                          <use xlink:href="#icon-bookmark"></use>
+                          <use xlinkHref="#icon-bookmark"></use>
                         </svg>
                         <span className="visually-hidden">To bookmarks</span>
                       </button>
                     </div>
                     <div className="place-card__rating rating">
                       <div className="place-card__stars rating__stars">
-                        <span style="width: 100%"></span>
+                        <span style={{width: '100%'}}></span>
                         <span className="visually-hidden">Rating</span>
                       </div>
                     </div>
@@ -229,7 +229,7 @@ function WelcomeScreen({errorsCount}: WelcomeScreenProps): JSX.Element {
                 <article className="cities__place-card place-card">
                   <div className="cities__image-wrapper place-card__image-wrapper">
                     <a href="#">
-                      <img className="place-card__image" src="img/room.jpg" width="260" height="200" alt="Place image">
+                      <img className="place-card__image" src="img/room.jpg" width="260" height="200" alt="Place image" />
                     </a>
                   </div>
                   <div className="place-card__info">
@@ -240,14 +240,14 @@ function WelcomeScreen({errorsCount}: WelcomeScreenProps): JSX.Element {
                       </div>
                       <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
                         <svg className="place-card__bookmark-icon" width="18" height="19">
-                          <use xlink:href="#icon-bookmark"></use>
+                          <use xlinkHref="#icon-bookmark"></use>
                         </svg>
                         <span className="visually-hidden">In bookmarks</span>
                       </button>
                     </div>
                     <div className="place-card__rating rating">
                       <div className="place-card__stars rating__stars">
-                        <span style="width: 80%"></span>
+                        <span style={{width: '80%'}}></span>
                         <span className="visually-hidden">Rating</span>
                       </div>
                     </div>
